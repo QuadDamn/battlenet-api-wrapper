1e1b61a183edcb2ea51314664aff42aa5948e4eb
Shane Jeffery <nobull.wow@gmail.com>
2020-02-12 21:26:24 -0800

    Stashing.

--- src/@types/wowProfileData.d.ts
+++ src/@types/wowProfileData.d.ts
@@ -15,34 +15,109 @@ declare module 'types/wowProfileData' {
     _links: TSelfLink;
   }
 
-  interface ICharacter {}
+  interface ICharacter {
+      key: TLink,
+      name: string,
+      id: number,
+      realm: object
+  }
 
-  interface IPlayableRace {}
+  interface IPlayableRace {
+      key: TLink,
+      name: string,
+      id: number
+  }
 
-  interface IPlayableClass {}
+  interface IPlayableClass {
+      key: TLink,
+      name: string,
+      id: number
+  }
 
-  interface IActiveSpec {}
+  interface IActiveSpec {
+      key: TLink,
+      name: string,
+      id: number
+  }
 
-  interface IGender {}
+  interface IGender {
+      type: string,
+      name: string
+  }
 
-  interface IFaction {}
+  interface IFaction {
+      type: string,
+      name: string
+  }
 
-  type TGuildCrestEmblem = {};
-
-  type TGuildCrestBorder = {};
-
-  type TMedia = {
-    id: number;
-    media: TKeyLink;
+  type TGuildCrestEmblem = {
+      id: number,
+      media: TKeyLink,
+      color: object
   };
 
-  type TGuildCrestBackground = {};
+  type TGuildCrestBorder = {
+      id: number,
+      media: TKeyLink,
+      color: object
+  };
 
-  interface IGuildCrest {}
+  interface IGuildCrest {
+      emblem: TGuildCrestEmblem,
+      border: TGuildCrestBorder,
+      background: object
+  }
 
-  interface ICharacterAppearance {}
+  interface ICharacterAppearance {
+      face_variation: number,
+      skin_color: number,
+      hair_variation: number,
+      hair_color: number,
+      feature_variation: number,
+      custom_display_options: number[]
+  }
 
-  interface ICharacterItems {}
+  interface ICharacterItem {
+      id: number,
+      slot: object,
+      enchant: number,
+      item_appearance_modifier_id: number,
+      internal_slot_id: number,
+      subclass: number
+  }
 
-  interface ICharacterAppearanceSummary {}
+  interface ICharacterItems {
+      items: ICharacterItem[]
+  }
+
+  export interface ICharacterAppearanceSummary extends IBaseResponse {
+      character: ICharacter,
+      playable_race: IPlayableRace,
+      playable_class: IPlayableClass,
+      active_spec: IActiveSpec,
+      gender: IGender,
+      faction: IFaction,
+      guild_Crest: IGuildCrest,
+      appearance: ICharacterAppearance,
+      items: ICharacterItems
+  }
+
+  export interface ICharacterCollections extends IBaseResponse {
+      pets: TLink,
+      mounts: TLink
+  }
+
+  interface ICharacterMount {
+      key: TLink,
+      name: string,
+      id: number
+  }
+
+  interface ICharacterMountContainer {
+      mount: ICharacterMount
+  }
+
+  export interface ICharacterMountCollection extends IBaseResponse {
+      mounts: ICharacterMountContainer[]
+  }
 }
